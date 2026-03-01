a469a30e5bca67da5b2a114b01485a869d38739e
Mahek Velani <mahekpatel613@gmail.com>
2026-03-02 02:37:06 +0530

    Formating code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
 import { DB_NAME } from "./constant.js";
 import connectDB from "./db/index.js";
+import dotenv from 'dotenv'
 
+
+// dotenv config
 dotenv.config({
     path: "./.env"
 })
 
 // 2nd Approch -- best Approch
-
-
-
 connectDB();
 
 
