217bb77b56ab5f8a117287c482ef1d239f17c217
Mahek Velani <mahekpatel613@gmail.com>
2026-03-03 02:08:50 +0530

    Handling Utilities

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+// 2nd Approch -- Best and Advance Approch
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) =>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+    }
+}
+
+
+
+// 1st Approch
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+        
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success : false,
+//             message : error.message
+//         })
+//     }
+// }
+export { asyncHandler };
